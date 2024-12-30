06206a1057e4cef0647c41367e8a5f93684b876a
Mike Oliphant <oliphant@nostatic.org>
2024-12-30 10:39:51 -0800

    Add detected drum notes to total

--- ChartPlayerShared/DrumPlayerScene3D.cs
+++ ChartPlayerShared/DrumPlayerScene3D.cs
@@ -81,6 +81,7 @@ namespace ChartPlayer
                     notesDetected[pos] = currentTime - note.TimeOffset;
 
                     NumNotesDetected++;
+                    NumNotesTotal++;
                 }
             }
         }
